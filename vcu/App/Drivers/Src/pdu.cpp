9eb3d752d2174b9eaa16ce319202e1a5eb6565a9
transparentglass2507 <242526231+transparentglass2507@users.noreply.github.com>
2025-11-13 17:16:10 -0800

    better formatting

--- vcu/App/Drivers/Src/pdu.cpp
+++ vcu/App/Drivers/Src/pdu.cpp
@@ -6,78 +6,73 @@
 #include <cstring>
 
 namespace drivers::PDU {
-PDU::PDU(drivers::CAN::CANBus& canBus)
-		: drivers::CAN::CANPeripheral<PDU, State>(canBus)
-{
+PDU::PDU(drivers::CAN::CANBus &canBus) :
+		drivers::CAN::CANPeripheral<PDU, State>(canBus) {
 	bindHandler<&PDU::processMessage1>(CAN_ID_RX_1);
 	bindHandler<&PDU::processMessage2>(CAN_ID_RX_2);
 }
 
-void PDU::setCurrentLimit(uint8_t channel, float amps)
-{
-	if(amps < 0) {
+void PDU::setCurrentLimit(uint8_t channel, float amps) {
+	if (amps < 0) {
 		return; // how did we get here?
 	}
-	if(channel != 0 && channel < NUM_CHANNELS) { // check channel in range
-		state.requestedCurrentLimit[channel-1] = std::clamp(amps,0.0f,255.0f);
-		txCurrentLimit[channel-1] = static_cast<uint8_t>(std::clamp(
-			std::round(amps*PDU_BIT_TO_POWER_SCALE),0.0f,255.0f)
-	);
-	canBus.transmit(CAN_ID_SET_CURRENT, txCurrentLimit, FDCAN_DLC_BYTES_8);
+	if (channel != 0 && channel < NUM_CHANNELS) { // check channel in range
+		state.requestedCurrentLimit[channel - 1] = std::clamp(amps, 0.0f,
+				255.0f); // accurately reflect max/min value sent to the PDU
+		txCurrentLimit[channel - 1] = static_cast<uint8_t>(std::clamp(
+				std::round(amps * PDU_BIT_TO_POWER_SCALE), 0.0f, 255.0f));
+		canBus.transmit(CAN_ID_SET_CURRENT, txCurrentLimit, FDCAN_DLC_BYTES_8);
 	}
 	return;
 }
-void PDU::setPWMDutyCycle(uint8_t channel, uint8_t dutyCyclePercent)
-{
-	if(channel != 0 && channel < NUM_CHANNELS) {
-		state.requestedPWMDutyPercent[channel-1] = dutyCyclePercent;
-		txPWM[channel-1] = static_cast<uint8_t>(std::clamp(
-			std::round(dutyCyclePercent*PDU_BIT_TO_POWER_SCALE),0.0,255.0)
-	);
-	canBus.transmit(CAN_ID_SET_PWM, txPWM, FDCAN_DLC_BYTES_8);
+void PDU::setPWMDutyCycle(uint8_t channel, uint8_t dutyCyclePercent) {
+	if (channel != 0 && channel < NUM_CHANNELS) {
+		state.requestedPWMDutyPercent[channel - 1] = dutyCyclePercent;
+		txPWM[channel - 1] = static_cast<uint8_t>(std::clamp(
+				std::round(dutyCyclePercent * PDU_BIT_TO_POWER_SCALE), 0.0,
+				255.0));
+		canBus.transmit(CAN_ID_SET_PWM, txPWM, FDCAN_DLC_BYTES_8);
 	}
 	return;
 }
 
-void PDU::processmessage(int channelStart, const CAN::Message& message){
-	if(message.numBytes != 8) {
+void PDU::processMessage(int channelStart, const CAN::Message &message) {
+	if (message.numBytes != 8) {
 		return; // incorrect number of bytes received, bad message
 	}
-	for(int i = 0;i<NUM_CHANNELS;i+=2) {
+	for (int i = 0; i < NUM_CHANNELS; i += 2) {
 		uint8_t errorCode = ((message.data[i] & errorMask) >> 5);
-		switch(errorCode) {
-			case 0x00:
-				state.errorStatuses[channelStart] = state.OK;
-				break;
-			case 0x01:
-				state.errorStatuses[channelStart] = state.OPEN_CIRCUIT;
-				break;
-			case 0x02:
-				state.errorStatuses[channelStart] = state.CURRENT_LIMIT_EXCEEDED;
-				break;
-			case 0x04:
-				state.errorStatuses[channelStart] = state.SHORT_CIRCUIT;
-				break;
-			default:
-				state.errorStatuses[channelStart] = state.UNKNOWN;
-				break;
+		switch (errorCode) {
+		case 0x00:
+			state.errorStatuses[channelStart] = state.OK;
+			break;
+		case 0x01:
+			state.errorStatuses[channelStart] = state.OPEN_CIRCUIT;
+			break;
+		case 0x02:
+			state.errorStatuses[channelStart] = state.CURRENT_LIMIT_EXCEEDED;
+			break;
+		case 0x04:
+			state.errorStatuses[channelStart] = state.SHORT_CIRCUIT;
+			break;
+		default:
+			state.errorStatuses[channelStart] = state.UNKNOWN;
+			break;
 		}
-		state.measuredCurrent[channelStart] = ((message.data[i]<<8) | message.data[i+1]) & currentMask;
+		state.measuredCurrent[channelStart] = ((message.data[i] << 8)
+				| message.data[i + 1]) & currentMask;
 		channelStart++;
 	}
 }
 
-void PDU::processMessage1(const CAN::Message& message)
-{
-	processmessage(RX_1_CHANNEL_BEGIN,message);
+void PDU::processMessage1(const CAN::Message &message) {
+	processMessage(RX_1_CHANNEL_BEGIN, message);
 }
-void PDU::processMessage2(const CAN::Message& message)
-{
-	processmessage(RX_2_CHANNEL_BEGIN,message);
+void PDU::processMessage2(const CAN::Message &message) {
+	processMessage(RX_2_CHANNEL_BEGIN, message);
 }
 
-void PDU::stopAllChannels()
-{
+void PDU::stopAllChannels() {
 	memset(state.requestedCurrentLimit, 0, NUM_CHANNELS); // set all bytes to 0
 	memset(state.requestedPWMDutyPercent, 0, NUM_CHANNELS);
 	memset(txCurrentLimit, 0, NUM_CHANNELS); // set all bytes to 0
