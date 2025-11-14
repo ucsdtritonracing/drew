ddbcb487cf06e4e0c853ddca508301f1cd78dcc6
nshie <njshie@gmail.com>
2025-11-13 18:34:46 -0800

    Changed CANBus private members and method parameters

--- vcu/App/Drivers/Inc/can_bus.hpp
+++ vcu/App/Drivers/Inc/can_bus.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "cmsis_os.h"
 #include "stm32g4xx_hal.h"
 
 namespace drivers::CAN {
@@ -12,7 +11,7 @@ struct HandlerEntry;
 using CANHandler = void (*)(void*, const Message&);
 
 struct Message {
-	FDCAN_RxHeaderTypeDef *rxHeader;
+	FDCAN_RxHeaderTypeDef rxHeader;
 	uint8_t numBytes;
 	uint8_t data[MAX_CLASSICAL_CAN_DATA_LENGTH];
 };
@@ -25,11 +24,11 @@ struct HandlerEntry {
 
 class CANBus {
 public:
-    CANBus(FDCAN_HandleTypeDef *fdcan, osMessageQueueId_t rxQueue);
+    CANBus(FDCAN_HandleTypeDef *fdcan) : fdcan(fdcan) {}
 
     void transmit(uint32_t id, const uint8_t *data, uint32_t dlc) const;
     void addMessageHandler(void *instance, uint32_t id, CANHandler callback);
-    void processMessages();
+    void processMessage(Message *message);
 
 private:
 	static constexpr size_t MAX_HANDLERS = 32;
@@ -37,7 +36,6 @@ private:
     size_t numHandlers = 0;
 
     FDCAN_HandleTypeDef* fdcan;
-    osMessageQueueId_t rxQueue;
 };
 
 } // namespace drivers::CAN
