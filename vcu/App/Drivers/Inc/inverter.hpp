5b9a941e45a0bd071553a5d89a2f2c80ae73761e
Emily <e2peng@ucsd.edu>
2025-12-23 18:41:15 -0800

    Temperature Messages Decoded. (might change implementation)

--- vcu/App/Drivers/Inc/inverter.hpp
+++ vcu/App/Drivers/Inc/inverter.hpp
@@ -5,14 +5,56 @@
 namespace drivers::inverter {
 
 struct State {
-
+	struct Temperatures {
+		int16_t moduleA;
+		int16_t moduleB;
+		int16_t moduleC;
+		int16_t gateDriverBoard;
+		int16_t controlBoard;
+		int16_t rtd1;
+		int16_t rtd2;
+		int16_t rtd3;
+		int16_t coolant;
+		int16_t hotSpot;
+		int16_t motor;
+	};
+	Temperatures temperatures;
+	int16_t torqueShudder;
 };
 
 class Inverter: public drivers::CAN::CANPeripheral<Inverter, State> {
 public:
+
 	Inverter(drivers::CAN::CANBus &canBus);
 
-	void commandMessage(uint16_t torqueRequest);
+	/*
+	 * @brief Send torque request to motor.
+	 *
+	 * @param torque request
+	 * @param inverter enabled or disabled
+	 */
+	void commandMessage(uint16_t torqueRequest, bool inverterEnable);
+
+	/*
+	 * @brief Process Temperature 1 messages
+	 *
+	 * @param CAN message
+	 */
+	void processTemperature1(const CAN::Message &message);
+
+	/*
+	 * @brief Process Temperature 2 messages
+	 *
+	 * @param CAN message
+	 */
+	void processTemperature2(const CAN::Message &message);
+
+	/*
+	 * @brief Process Temperature 3 messages
+	 *
+	 * @param CAN message
+	 */
+	void processTemperature3(const CAN::Message &message);
 
 private:
 
@@ -21,8 +63,6 @@ private:
 	static constexpr uint32_t COMMAND_MESSAGE_ID = 0x0C0;
 	static constexpr uint8_t TORQUE_SCALE = 10;
 	static constexpr uint8_t DIRECTION_COMMAND = 1; // Forward
-	static constexpr uint8_t INVERTER_ON = 1;
-	static constexpr uint8_t INVERTER_OFF = 0;
 	static constexpr uint8_t INVERTER_DISCHARGE = 0; // Discharge disabled
 	static constexpr uint8_t SPEED_MODE_ENABLE = 0; // Do not override Torque Mode
 	// Set motor to max speed of 4100 RPM
@@ -31,6 +71,10 @@ private:
 	// Set motor torque limit to 210 N.m --> 210 * 10 = 2100 (scaled)
 	static constexpr uint8_t COMMANDED_TORQUE_LIMIT_BYTE_6 = 0b00110100;
 	static constexpr uint8_t COMMANDED_TORQUE_LIMIT_BYTE_7 = 0b00001000;
+
+	static constexpr uint32_t CAN_ID_TEMP_1 = 0x0A0;
+	static constexpr uint32_t CAN_ID_TEMP_2 = 0x0A1;
+	static constexpr uint32_t CAN_ID_TEMP_3 = 0x0A2;
 
 };
 
--- vcu/App/Drivers/Src/inverter.cpp
+++ vcu/App/Drivers/Src/inverter.cpp
@@ -5,10 +5,12 @@
 namespace drivers::inverter {
 	Inverter::Inverter(drivers::CAN::CANBus &canBus) :
 			drivers::CAN::CANPeripheral<Inverter, State>(canBus) {
-		//fill later
+		bindHandler<&Inverter::processTemperature1>(CAN_ID_TEMP_1);
+		bindHandler<&Inverter::processTemperature2>(CAN_ID_TEMP_2);
+		bindHandler<&Inverter::processTemperature3>(CAN_ID_TEMP_3);
 	}
 
-	void Inverter::commandMessage(uint16_t torqueRequest) {
+	void Inverter::commandMessage(uint16_t torqueRequest, bool inverterEnable) {
 		uint16_t scaledTorque = torqueRequest * TORQUE_SCALE;
 		if (scaledTorque > 2100) scaledTorque = 2100; // If too redundant, delete
 		txData[0] = scaledTorque & 0xFF;
@@ -16,10 +18,41 @@ namespace drivers::inverter {
 		txData[2] = SPEED_COMMAND_BYTE_2;
 		txData[3] = SPEED_COMMAND_BYTE_3;
 		txData[4] = DIRECTION_COMMAND;
-		txData[5] = INVERTER_ON | (INVERTER_DISCHARGE << 1) | (SPEED_MODE_ENABLE << 2);
+		txData[5] = inverterEnable | (INVERTER_DISCHARGE << 1) | (SPEED_MODE_ENABLE << 2);
 		txData[6] = COMMANDED_TORQUE_LIMIT_BYTE_6;
 		txData[7] = COMMANDED_TORQUE_LIMIT_BYTE_7;
 		canBus.transmit(COMMAND_MESSAGE_ID, txData, FDCAN_DLC_BYTES_8);
 	}
 
+	void Inverter::processTemperature1(const CAN::Message &message) {
+		state.temperatures.moduleA = ((uint16_t)message.data[1] << 8) | message.data[0];
+		state.temperatures.moduleB = ((uint16_t)message.data[3] << 8) | message.data[2];
+		state.temperatures.moduleC = ((uint16_t)message.data[5] << 8) | message.data[4];
+		state.temperatures.gateDriverBoard = ((uint16_t)message.data[7] << 8) | message.data[6];
+	}
+
+	void Inverter::processTemperature2(const CAN::Message &message) {
+		state.temperatures.controlBoard = ((uint16_t)message.data[1] << 8) | message.data[0];
+		state.temperatures.rtd1 = ((uint16_t)message.data[3] << 8) | message.data[2];
+		state.temperatures.rtd2 = ((uint16_t)message.data[5] << 8) | message.data[4];
+		state.temperatures.rtd3 = ((uint16_t)message.data[7] << 8) | message.data[6];
+	}
+
+	void Inverter::processTemperature3(const CAN::Message &message) {
+		state.temperatures.coolant = ((uint16_t)message.data[1] << 8) | message.data[0];
+		state.temperatures.hotSpot = ((uint16_t)message.data[3] << 8) | message.data[2];
+		state.temperatures.motor = ((uint16_t)message.data[5] << 8) | message.data[4];
+		state.torqueShudder = ((uint16_t)message.data[7] << 8) | message.data[6];
+	}
+
 } // namespace drivers::inverter
+/*
+ * static constexpr size_t NUM_TEMPS = 4;
+ * std::array<int16_t, NUM_TEMPS> Inverter::processTemperature(const CAN::Message &message) {
+		std::array<int16_t, NUM_TEMPS> temperatures;
+		for (int i = 0; i < NUM_TEMPS; i++) {
+			temperatures[i] = (int16_t)(((uint16_t)message.data[i*2+1] << 8) | message.data[i*2]);
+		}
+		return temperatures;
+	}
+ */
