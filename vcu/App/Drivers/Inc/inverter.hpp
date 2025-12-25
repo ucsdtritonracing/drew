238b687d0bbe12bdd6d51fed219687059647726b
Emily <e2peng@ucsd.edu>
2025-12-24 21:55:18 -0800

    Completed 1st Iteration of Inverter Driver.

--- vcu/App/Drivers/Inc/inverter.hpp
+++ vcu/App/Drivers/Inc/inverter.hpp
@@ -1,22 +1,65 @@
 #pragma once
 #include "can_bus.hpp"
 #include "can_peripheral.hpp"
-#include <array>
+#include <type_traits>
+#include <cassert>
 
 namespace drivers::inverter {
 
-enum class Temperatures : uint8 {
+enum class Temperatures : uint8_t {
 	ModuleA, ModuleB, ModuleC, GateDriverBoard, ControlBoard,
 	RTD1, RTD2, RTD3, Coolant, HotSpot, Motor, TorqueShudder, Count
 };
 
-enum class MotorPosition : uint8 {
+enum class MotorPosition : uint8_t {
 	MotorAngle, MotorSpeed, ElectricalOutputFrequency, DeltaResolverFiltered, Count
 };
 
+enum class TorqueInformation : uint8_t {
+	CommandedTorque, TorqueFeedback, TorqueCapability, Count
+};
+
+enum class FaultCodes : uint8_t {
+	PostFaultLo, PostFaultHi, RunFaultLo, RunFaultHi, Count
+};
+
+enum class PostFaultLo : uint8_t {
+	HardwareGate_Desaturation, HardwareOvercurrent, AcceleratorShorted, AcceleratorOpen,
+	CurrentSensorLow, CurrentSensorHigh, ModuleTempLow, ModuleTempHigh,
+	ControlPCBTempLow, ControlPCBTempHigh, GateDrivePCBTempLow, GateDrivePCBTempHigh,
+	Sense5V_Low, Sense5V_High, Sense12V_Low, Sense12V_High
+};
+
+enum class PostFaultHi : uint8_t {
+	Sense2p5V_Low, Sense2p5V_High, Sense1p5_Low, Sense1p5_High,
+	DC_BusVoltageHigh, DC_BusVoltageLow, PrechargeTimeout, PrechargeVoltageFailure,
+	EEPROMChecksumInvalid, EEPROMDataOutOfRange, EEPROMUpdateRequired, HardwareDC_BusOvervoltage,
+	BrakeShorted = 14, BrakeOpen = 15
+};
+
+enum class RunFaultLo : uint8_t {
+	MotorOverspeed, Overcurrent, Overvoltage, InverterOverTemp,
+	AcceleratorInputShorted, AcceleratorInputOpen, DirectionCommand, InverterResponseTimeout,
+	HardwareGate_Desaturation, HardwareOvercurrent, Undervoltage, CANCommandMessageLost,
+	MotorOverTemp
+};
+
+enum class RunFaultHi : uint8_t {
+	BrakeInputShorted, BrakeInputOpen, ModuleAOvertemp, ModuleBOvertemp,
+	ModuleCOvertemp, PCBOvertemp, GateDriveBoard1Overtemp, GateDriveBoard2Overtemp,
+	GateDriveBoard3Overtemp, CurrentSensor, ResolverNotConnected = 14
+};
+
+enum class InternalStates : uint8_t {
+	InverterEnableState, InverterEnableLockout, BMSActive, BMSLimitingTorque, Count
+};
+
 struct State {
-	std::array<int16_t, static_cast<size_t>(Temperatures::Count)> temperatures;
-	std::array<int16_t, static_cast<size_t>(MotorPosition::Count)> motorPosition;
+	float temperatures[static_cast<size_t>(Temperatures::Count)];
+	float motorPosition[static_cast<size_t>(MotorPosition::Count)];
+	float torqueInformation[static_cast<size_t>(TorqueInformation::Count)];
+	uint16_t faultCodes[static_cast<size_t>(FaultCodes::Count)];
+	bool internalStates[static_cast<size_t>(InternalStates::Count)];
 };
 
 class Inverter: public drivers::CAN::CANPeripheral<Inverter, State> {
@@ -60,12 +103,53 @@ public:
 	 */
 	void processMotorPosition(const CAN::Message &message);
 
+	/*
+	 * @brief Process Torque Information message
+	 *
+	 * @param CAN message
+	 */
+	void processTorqueInformation(const CAN::Message &message);
+
+	/*
+	 * @brief Process Torque Capability message
+	 *
+	 * @param CAN message
+	 */
+	void processTorqueCapability(const CAN::Message &message);
+
+	/*
+	 * @brief Process Fault Codes message
+	 *
+	 * @param CAN message
+	 */
+	void processFaultCodes(const CAN::Message &message);
+
+	/*
+	 * @brief Returns if a specific fault has occured
+	 *
+	 * @param Category of fault (4 codes)
+	 * @param Specific fault in category (16 faults per code)
+	 */
+	template<typename Enum>
+	bool findFault(FaultCodes faultCode, Enum faultName) {
+		static_assert(std::is_enum_v<Enum>, "Must input an enum.");
+		return state.faultCodes[static_cast<size_t>(faultCode)] & static_cast<uint16_t>(1u << static_cast<uint8_t>(faultName));
+	}
+
+	/*
+	 * @brief Process Internal States message
+	 *
+	 * @param CAN message
+	 */
+	void processInternalStates(const CAN::Message &message);
+
 private:
 
 	uint8_t txData[drivers::CAN::MAX_CLASSICAL_CAN_DATA_LENGTH] = {};
+	static constexpr uint8_t COMMON_SCALE = 10; //temperature, torque, angle, frequency
+	static constexpr uint8_t NO_SCALE = 1; // angular velocity, internal
 
 	static constexpr uint32_t COMMAND_MESSAGE_ID = 0x0C0;
-	static constexpr uint8_t TORQUE_SCALE = 10;
 	static constexpr uint8_t DIRECTION_COMMAND = 1; // Forward
 	static constexpr uint8_t INVERTER_DISCHARGE = 0; // Discharge disabled
 	static constexpr uint8_t SPEED_MODE_ENABLE = 0; // Do not override Torque Mode
@@ -76,21 +160,54 @@ private:
 	static constexpr uint8_t COMMANDED_TORQUE_LIMIT_BYTE_6 = 0b00110100;
 	static constexpr uint8_t COMMANDED_TORQUE_LIMIT_BYTE_7 = 0b00001000;
 
-	static constexpr size_t NUM_DATA = 4;
-	static constexpr int DATA_1_BEGIN = 0;
-	static constexpr int DATA_2_BEGIN = 4;
-	static constexpr int DATA_3_BEGIN = 8;
-
 	static constexpr uint32_t CAN_ID_TEMP_1 = 0x0A0;
 	static constexpr uint32_t CAN_ID_TEMP_2 = 0x0A1;
 	static constexpr uint32_t CAN_ID_TEMP_3 = 0x0A2;
+	static constexpr size_t TEMP_1_START = static_cast<size_t>(Temperatures::ModuleA);
+	static constexpr size_t TEMP_2_START = static_cast<size_t>(Temperatures::ControlBoard);
+	static constexpr size_t TEMP_3_START = static_cast<size_t>(Temperatures::Coolant);
 
 	static constexpr uint32_t CAN_ID_MOTOR_POSITION = 0x0A5;
+	static constexpr size_t MOTOR_POSITION_START = static_cast<size_t>(MotorPosition::MotorAngle);
 
-	template <size_t dataLength>
-	void processStandardMessage(const CAN::Message &message, std::array<int16_t, dataLength> &data, int dataStart) {
-		for (int i = 0; i < NUM_DATA; i++) {
-			data[dataStart + i] = ((uint16_t)message.data[i*2+1] << 8) | message.data[i*2];
+	static constexpr uint32_t CAN_ID_TORQUE_INFORMATION = 0x0AC;
+	static constexpr uint32_t CAN_ID_TORQUE_CAPABILITY = 0x0B1;
+	static constexpr size_t TORQUE_INFO_START = static_cast<size_t>(TorqueInformation::CommandedTorque);
+	static constexpr size_t TORQUE_CAP_START = static_cast<size_t>(TorqueInformation::TorqueCapability);
+
+	static constexpr uint32_t CAN_ID_FAULT_CODES = 0x0AB;
+	static constexpr size_t FAULT_START = static_cast<size_t>(FaultCodes::PostFaultLo);
+
+	static constexpr uint32_t CAN_ID_INTERNAL_STATES = 0x0AA;
+	static constexpr uint8_t INVERTER_ENABLE_STATE_MASK = 0x01;
+	static constexpr uint8_t INVERTER_ENABLE_LOCKOUT_MASK = 0x80;
+	static constexpr uint8_t BMS_ACTIVE_MASK = 0x02;
+	static constexpr uint8_t BMS_LIMITING_TORQUE_MASK = 0x04;
+
+	static constexpr size_t ONE_DATA = 1;
+	static constexpr size_t TWO_DATA = 2;
+	static constexpr size_t FOUR_DATA = 4;
+	static constexpr uint8_t MOTOR_POSITION_SCALES[FOUR_DATA] = {COMMON_SCALE, NO_SCALE, COMMON_SCALE, COMMON_SCALE};
+
+	template <size_t dataLength, typename Data>
+	void processStandardMessage(const CAN::Message &message, size_t numData, Data (&data)[dataLength], size_t dataStart, uint8_t scale) {
+		if (message.numBytes != 8) {
+			return; // incorrect number of bytes received, bad message
+		}
+		assert(dataStart + numData <= dataLength);
+		for (size_t i = 0; i < numData; i++) {
+			data[dataStart + i] = static_cast<Data>(static_cast<int16_t>((static_cast<uint16_t>(message.data[i*2+1]) << 8) | message.data[i*2])) / scale;
+		}
+	}
+
+	template <size_t dataLength, size_t scalerLength, typename Data>
+	void processStandardMessage(const CAN::Message &message, size_t numData, Data (&data)[dataLength], size_t dataStart, const uint8_t (&scale)[scalerLength]) {
+		if (message.numBytes != 8) {
+			return; // incorrect number of bytes received, bad message
+		}
+		assert(dataStart + numData <= dataLength);
+		for (size_t i = 0; i < numData; i++) {
+			data[dataStart + i] = static_cast<Data>(static_cast<int16_t>((static_cast<uint16_t>(message.data[i*2+1]) << 8) | message.data[i*2])) / scale[i];
 		}
 	}
 
--- vcu/App/Drivers/Src/inverter.cpp
+++ vcu/App/Drivers/Src/inverter.cpp
@@ -9,10 +9,14 @@ namespace drivers::inverter {
 		bindHandler<&Inverter::processTemperature2>(CAN_ID_TEMP_2);
 		bindHandler<&Inverter::processTemperature3>(CAN_ID_TEMP_3);
 		bindHandler<&Inverter::processMotorPosition>(CAN_ID_MOTOR_POSITION);
+		bindHandler<&Inverter::processTorqueInformation>(CAN_ID_TORQUE_INFORMATION);
+		bindHandler<&Inverter::processTorqueCapability>(CAN_ID_TORQUE_CAPABILITY);
+		bindHandler<&Inverter::processFaultCodes>(CAN_ID_FAULT_CODES);
+		bindHandler<&Inverter::processInternalStates>(CAN_ID_INTERNAL_STATES);
 	}
 
 	void Inverter::commandMessage(uint16_t torqueRequest, bool inverterEnable) {
-		uint16_t scaledTorque = torqueRequest * TORQUE_SCALE;
+		uint16_t scaledTorque = torqueRequest * COMMON_SCALE;
 		if (scaledTorque > 2100) scaledTorque = 2100; // If too redundant, delete
 		txData[0] = scaledTorque & 0xFF;
 		txData[1] = (scaledTorque >> 8) & 0xFF;
@@ -26,19 +30,42 @@ namespace drivers::inverter {
 	}
 
 	void Inverter::processTemperature1(const CAN::Message &message) {
-		processStandardMessage(message, state.temperatures, DATA_1_BEGIN);
+		processStandardMessage(message, FOUR_DATA, state.temperatures, TEMP_1_START, COMMON_SCALE);
 	}
 
 	void Inverter::processTemperature2(const CAN::Message &message) {
-		processStandardMessage(message, state.temperatures, DATA_2_BEGIN);
+		processStandardMessage(message, FOUR_DATA, state.temperatures, TEMP_2_START, COMMON_SCALE);
 	}
 
 	void Inverter::processTemperature3(const CAN::Message &message) {
-		processStandardMessage(message, state.temperatures, DATA_3_BEGIN);
+		processStandardMessage(message, FOUR_DATA, state.temperatures, TEMP_3_START, COMMON_SCALE);
 	}
 
 	void Inverter::processMotorPosition(const CAN::Message &message) {
-		processStandardMessage(message, state.motorPosition, DATA_1_BEGIN);
+		processStandardMessage(message, FOUR_DATA, state.motorPosition, MOTOR_POSITION_START, MOTOR_POSITION_SCALES);
+	}
+
+	void Inverter::processTorqueInformation(const CAN::Message &message) {
+		processStandardMessage(message, TWO_DATA, state.torqueInformation, TORQUE_INFO_START, COMMON_SCALE);
+	}
+
+	void Inverter::processTorqueCapability(const CAN::Message &message) {
+		processStandardMessage(message, ONE_DATA, state.torqueInformation, TORQUE_CAP_START, COMMON_SCALE);
+	}
+
+	void Inverter::processFaultCodes(const CAN::Message &message) {
+		processStandardMessage(message, FOUR_DATA, state.faultCodes, FAULT_START, NO_SCALE);
+	}
+
+	void Inverter::processInternalStates(const CAN::Message &message) {
+		if (message.numBytes != 8) {
+			return; // incorrect number of bytes received, bad message
+		}
+		// Hard-coded b/c it takes least runtime and storage
+		state.internalStates[0] = message.data[6] & INVERTER_ENABLE_STATE_MASK;
+		state.internalStates[1] = message.data[6] & INVERTER_ENABLE_LOCKOUT_MASK;
+		state.internalStates[2] = message.data[7] & BMS_ACTIVE_MASK;
+		state.internalStates[3] = message.data[7] & BMS_LIMITING_TORQUE_MASK;
 	}
 
 } // namespace drivers::inverter
