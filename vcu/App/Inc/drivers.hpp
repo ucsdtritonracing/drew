9dc739a1315c0867e36d6bf7408dd86c85f6687d
nshie <njshie@gmail.com>
2025-12-28 00:47:02 -0800

    Added global vehicle state and drivers

---
+++ vcu/App/Inc/drivers.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "can_bus.hpp"
+#include "pdu.hpp"
+#include "steering_angle_sensor.hpp"
+
+extern drivers::can::CANBus CANBus1;
+extern drivers::can::CANBus CANBus2;
+extern drivers::pdu::PDU pdu;
+extern drivers::sas::SteeringAngleSensor sas;
