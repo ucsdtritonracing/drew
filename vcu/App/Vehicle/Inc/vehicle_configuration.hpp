476046d141a790849f054da0e4ddf4cef467376b
nshie <njshie@gmail.com>
2026-01-07 16:56:46 -0800

    Fixed PDU configuration declarations

--- vcu/App/Vehicle/Inc/vehicle_configuration.hpp
+++ vcu/App/Vehicle/Inc/vehicle_configuration.hpp
@@ -4,12 +4,12 @@
 namespace vehicle {
 
 struct VehicleConfiguration {
-	constexpr uint8_t PDU_12V_CHANNEL 			= 1;
-	constexpr uint8_t PDU_TSSI_RED_CHANNEL 		= 2;
-	constexpr uint8_t PDU_TSSI_GREEN_CHANNEL 	= 3;
-	constexpr uint8_t PDU_BRAKE_LIGHT_CHANNEL 	= 4;
-	constexpr uint8_t PDU_TSB_FANS_CHANNEL 		= 5;
-	constexpr uint8_t PDU_PUMPS_CHANNEL 		= 6;
+	static constexpr uint8_t PDU_12V_CHANNEL 			= 1;
+	static constexpr uint8_t PDU_TSSI_RED_CHANNEL 		= 2;
+	static constexpr uint8_t PDU_TSSI_GREEN_CHANNEL 	= 3;
+	static constexpr uint8_t PDU_BRAKE_LIGHT_CHANNEL 	= 4;
+	static constexpr uint8_t PDU_TSB_FANS_CHANNEL 		= 5;
+	static constexpr uint8_t PDU_PUMPS_CHANNEL 			= 6;
 };
 
 extern VehicleConfiguration vehicleConfiguration;
