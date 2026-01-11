1a2beddcd32f92bc7a106496b75e0b2cbd5e904e
nshie <njshie@gmail.com>
2026-01-10 17:13:29 -0800

    Added getters/setters to vehicle state

--- vcu/App/Vehicle/Inc/vehicle_state.hpp
+++ vcu/App/Vehicle/Inc/vehicle_state.hpp
@@ -1,38 +1,113 @@
 #pragma once
 
 #include "stdint.h"
+#include "cmath"
 
 
 namespace vehicle {
 
 class VehicleState {
 public:
-	VehicleState();
-
-	// High sample rate sensors
-	struct WheelSpeed {
+	struct WheelSpeeds {
 		float frontLeft;
 		float frontRight;
 		float rearLeft;
 		float rearRight;
-	} wheelSpeeds;
+	};
 
 	struct AcceleratorPedalPositions {
 		uint16_t app1;
 		uint16_t app2;
-	} acceleratorPedalPositions;
+	};
 
 	struct BrakePressures {
 		uint16_t front;
 		uint16_t rear;
-	} brakePressures;
+	};
+
+	VehicleState();
+
+	const WheelSpeeds getWheelSpeeds() const {
+		return wheelSpeeds;
+	}
+
+	const AcceleratorPedalPositions getAcceleratorPedalPositions() const {
+		return acceleratorPedalPositions;
+	}
+
+	const BrakePressures getBrakePressures() const {
+		return brakePressures;
+	}
+
+	float getSteeringAngleDegrees() const {
+		return steeringAngleDegrees;
+	}
+
+	bool getReadyToDriveButtonPressed() const {
+		return readyToDriveButtonPressed;
+	}
+
+	bool getShutdownCircuitClosed() const {
+		return shutdownCircuitClosed;
+	}
+
+    bool getReadyToDrive() const {
+    	return readyToDrive;
+    }
+
+    void setWheelSpeedFL(float wheelSpeed) {
+    	wheelSpeeds.frontLeft = std::isfinite(wheelSpeed) ? wheelSpeed : 0;
+    }
+
+    void setWheelSpeedFR(float wheelSpeed) {
+    	wheelSpeeds.frontRight = std::isfinite(wheelSpeed) ? wheelSpeed : 0;
+    }
+
+    void setWheelSpeedRL(float wheelSpeed) {
+    	wheelSpeeds.rearLeft = std::isfinite(wheelSpeed) ? wheelSpeed : 0;
+    }
+
+    void setWheelSpeedRR(float wheelSpeed) {
+    	wheelSpeeds.rearRight = std::isfinite(wheelSpeed) ? wheelSpeed : 0;
+    }
+
+    void setAcceleratorPedalPositions(uint16_t app1, uint16_t app2) {
+    	acceleratorPedalPositions.app1 = app1;
+    	acceleratorPedalPositions.app2 = app2;
+    }
+
+    void setBrakePressures(uint16_t front, uint16_t rear) {
+    	brakePressures.front = front;
+    	brakePressures.rear = rear;
+    }
+
+	void setSteeringAngleDegrees(float degrees) {
+		steeringAngleDegrees = std::isfinite(degrees) ? degrees : 0;;
+	}
+
+	void setReadyToDriveButtonPressed(bool status) {
+		readyToDriveButtonPressed = status;
+	}
+
+	void setShutdownCircuitClosed(bool status) {
+		shutdownCircuitClosed = status;
+	}
+
+	void setReadyToDrive(bool status) {
+    	readyToDrive = status;
+    }
+
+private:
+	// High sample rate sensors
+	WheelSpeeds wheelSpeeds;
+
+	AcceleratorPedalPositions acceleratorPedalPositions;
+	BrakePressures brakePressures;
 
     float steeringAngleDegrees;
 
 
     // Polled signals
-	bool bmsFault;
-	bool imdFault;
 	bool readyToDriveButtonPressed;
 	bool shutdownCircuitClosed;
 
