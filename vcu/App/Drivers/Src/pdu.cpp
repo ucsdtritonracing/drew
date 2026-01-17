cbe6cc79a9b4a3e26a71208f009368cb3dfbac64
nshie <65916936+nshie@users.noreply.github.com>
2026-01-16 19:22:56 -0800

    Separate PDU parameters and commands (#9)
    Split up responsibility of setting parameters and sending a command message. Setting parameters no longer sends the command message, and it is also possible to send command message without tweaking parameters. This enables application-level code to have one task that constantly sends commands at a consistent rate while other tasks can control what gets sent.

--- vcu/App/Drivers/Src/pdu.cpp
+++ vcu/App/Drivers/Src/pdu.cpp
@@ -16,11 +16,11 @@ void PDU::init() {
 }
 
 void PDU::setCurrentLimit(uint8_t channel, float amps) {
-	float PDU_MAX_CURRENT; // assume 20A channel, check later
+	float PDU_MAX_CURRENT;
 	if (amps < 0) {
 		return; // how did we get here?
 	}
-	if (channel != 0 && channel < NUM_CHANNELS) { // check channel in range
+	if (channel != 0 && channel <= NUM_CHANNELS) { // check channel in range
 		PDU_MAX_CURRENT = CHANNEL_MASK & (1 << (channel - 1)) ? LOW_CURRENT_LIMIT : HIGH_CURRENT_LIMIT; // set to 10A if 1, 20 if 0
 		state.requestedCurrentLimit[channel - 1] = std::min(amps, PDU_MAX_CURRENT);
 		for (int i = 0; i < NUM_CHANNELS; i++) {
@@ -28,18 +28,31 @@ void PDU::setCurrentLimit(uint8_t channel, float amps) {
 				std::round(state.requestedCurrentLimit[i] * PDU_BIT_TO_POWER_SCALE)
 				);
 		}
-		canBus.transmit(CAN_ID_SET_CURRENT, txData, FDCAN_DLC_BYTES_8);
 	}
 }
 void PDU::setPWMDutyCycle(uint8_t channel, uint8_t dutyCyclePercent) {
-	if (channel != 0 && channel < NUM_CHANNELS) {
+	if (channel != 0 && channel <= NUM_CHANNELS) {
 		state.requestedPWMDutyPercent[channel - 1] = std::min(dutyCyclePercent, PDU_MAX_PWM);
 		for (int i = 0; i < NUM_CHANNELS; i++) {
 			txData[i] = static_cast<uint8_t>(
 				std::round(state.requestedPWMDutyPercent[i] * PDU_BIT_TO_POWER_SCALE)
 				);
 		}
+	}
+}
+
+void PDU::sendCommand(CommandMode mode) {
+	switch (mode) {
+	case CurrentLimit:
+		memcpy(txData, state.requestedCurrentLimit, drivers::CAN::MAX_CLASSICAL_CAN_DATA_LENGTH);
+		canBus.transmit(CAN_ID_SET_CURRENT, txData, FDCAN_DLC_BYTES_8);
+		break;
+	case PWM:
+		memcpy(txData, state.requestedCurrentLimit, drivers::CAN::MAX_CLASSICAL_CAN_DATA_LENGTH);
 		canBus.transmit(CAN_ID_SET_PWM, txData, FDCAN_DLC_BYTES_8);
+		break;
+	default:
+		return; // bad mode, reject
 	}
 }
 
