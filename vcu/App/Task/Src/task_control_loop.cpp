db336cce03a18cc41db964eab8b39e6fb92b59ae
nshie <njshie@gmail.com>
2026-01-16 16:21:22 -0800

    Added control loop task outline

--- vcu/App/Task/Src/task_control_loop.cpp
+++ vcu/App/Task/Src/task_control_loop.cpp
@@ -1,6 +1,8 @@
 #include "task.hpp"
 #include "task_control_loop.hpp"
 #include "vehicle_state.hpp"
+#include "drivers.hpp"
+#include "torque.hpp"
 #include "cmsis_os.h"
 #include <cmath>
 #include <algorithm>
@@ -8,18 +10,23 @@
 namespace tasks {
 
 void ControlLoopTask::loop() {
-	if (vehicle::vehicleState.readyToDrive) {
-		float torque; /* = computeDriverTorqueRequest(
-				vehicle::vehicleState.getAcceleratorPedalPositions(),
-				vehicle::inverter.getMotorRPM());
-				*/
+	if (vehicle::vehicleState.getReadyToDrive()) {
+		float torqueScalar = 0.0f;
 
-		if (!std::isfinite(torque)) {
-			torque = 0.0f;
+		if (torque::isAPPSPlausible(vehicle::vehicleState.getAcceleratorPedalPositions())) {
+			torqueScalar = torque::computeDriverTorqueRequest(
+					vehicle::vehicleState.getAcceleratorPedalPositions(),
+					vehicle::inverter.getMotorRPM()
+			);
 		}
 
-		torque = std::clamp(torque, 0.0f, 1.0f);
-		// TODO: vehicle::inverter.sendCommandMessage(torque, inverterEnabled = true);
+
+		if (!std::isfinite(torqueScalar)) {
+			torqueScalar = 0.0f;
+		}
+
+		torqueScalar = std::clamp(torqueScalar, 0.0f, 1.0f);
+		// TODO: vehicle::inverter.sendCommandMessage(torqueScalar * limit?, inverterEnabled = true);
 	} else {
 		// TODO: send torque request 0 Nm (or inverterEnabled = false...?)
 	}
