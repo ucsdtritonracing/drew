95a2130c41ac691808f3b9ef39a1bbf955a8e46d
Emily <e2peng@ucsd.edu>
2026-01-17 17:26:13 -0800

    computeDriverTorqueRequest done

--- vcu/App/Vehicle/Inc/torque.hpp
+++ vcu/App/Vehicle/Inc/torque.hpp
@@ -5,13 +5,15 @@
 
 namespace torque {
 
+static constexpr uint16_t MAX_TORQUE_LIMIT = 90;	// N.m
+
 /*
  * @brief Compute the driver torque request as a scalar from 0 to 1
  *
  * @param apps Accelerator pedal positions
  * @param motorRPM Motor speed in RPM
  */
-float computeDriverTorqueRequest(vehicle::VehicleState::AcceleratorPedalPositions apps, float motorRPM);
+uint16_t computeDriverTorqueRequest(vehicle::VehicleState::AcceleratorPedalPositions apps, float torqueCapability);
 
 /*
  * @brief Compute whether accelerator pedal positions are plausible
--- vcu/App/Vehicle/Src/torque.cpp
+++ vcu/App/Vehicle/Src/torque.cpp
@@ -1,12 +1,12 @@
 #include "torque.hpp"
 #include "vehicle_state.hpp"
-
+#include <algorithm>
 
 namespace torque {
 
-float computeDriverTorqueRequest(vehicle::VehicleState::AcceleratorPedalPositions apps, float motorRPM) {
-	// TODO: Compute driver torque request / pedal mapping
-	return 0;
+uint16_t computeDriverTorqueRequest(vehicle::VehicleState::AcceleratorPedalPositions apps, float torqueCapability) {
+	uint16_t torqueRequestNm = static_cast<uint16_t>(std::lroundf(std::min(apps.app1 * MAX_TORQUE_LIMIT, torqueCapability)));
+	return torqueRequestNm;
 }
 
 bool isAPPSPlausible(vehicle::VehicleState::AcceleratorPedalPositions apps) {
