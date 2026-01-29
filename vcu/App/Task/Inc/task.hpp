3cea11a069fbf92a4f1aa1f69db27ba6878cea35
nshie <njshie@gmail.com>
2026-01-28 16:54:37 -0800

    Updated task base class to use static memory

--- vcu/App/Task/Inc/task.hpp
+++ vcu/App/Task/Inc/task.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "cmsis_os.h"
+#include "freertos.h"
 #include <type_traits>
+
 
 namespace tasks {
 
@@ -23,19 +25,20 @@ class Task {
 public:
 	Task() = default;
 
-	Task(Task&&) 					= delete;
-	Task(const Task&)				= delete;
-	Task& operator=(const Task&) 	= delete;
-	Task& operator=(Task&&) 		= delete;
-
 	void start(const char* name) {
 		if (taskHandle) {
 			return;
 		}
-		osThreadAttr_t attr = {};
-		attr.name = name;
-		attr.stack_size = StackSize;
-		attr.priority = Priority;
+
+		const osThreadAttr_t attr = {
+		  .name = name,
+		  .attr_bits = osThreadDetached,
+		  .cb_mem = &tcb,
+		  .cb_size = sizeof(StaticTask_t),
+		  .stack_mem = stack,
+		  .stack_size = sizeof(stack),
+		  .priority = osPriorityNormal,
+		};
 		taskHandle = osThreadNew(taskEntry, this, &attr);
 	}
 
@@ -47,6 +50,9 @@ protected:
 	osThreadId_t taskHandle = nullptr;
 
 private:
+	StaticTask_t tcb;
+	StackType_t stack[StackSize / sizeof(StackType_t)];
+
 	static void taskEntry(void* arg) {
 		Derived* self = static_cast<Derived*>(arg);
 
