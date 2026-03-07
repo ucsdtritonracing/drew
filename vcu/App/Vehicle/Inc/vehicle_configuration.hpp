70e710f733ef85a037e25adcccdde6b06d05ec0d
nshie <njshie@gmail.com>
2026-03-06 19:27:14 -0800

    Add APPS voltage lo and hi constants

--- vcu/App/Vehicle/Inc/vehicle_configuration.hpp
+++ vcu/App/Vehicle/Inc/vehicle_configuration.hpp
@@ -11,10 +11,20 @@ class VehicleConfiguration {
 	static constexpr uint8_t PDU_TSB_FANS_CHANNEL 		= 5;
 	static constexpr uint8_t PDU_PUMPS_CHANNEL 			= 6;
 
+	static constexpr float INITIAL_APPS1_LO_THRESHOLD_VOLTAGE 	= 0.6f;
+	static constexpr float INITIAL_APPS1_HI_THRESHOLD_VOLTAGE 	= 3.1f;
+	static constexpr float INITIAL_APPS2_LO_THRESHOLD_VOLTAGE 	= 0.2f;
+	static constexpr float INITIAL_APPS2_HI_THRESHOLD_VOLTAGE 	= 2.7f;
+
 	static constexpr float INITIAL_BSEF_BRAKE_ENGAGED_THRESHOLD = 0.25;
 	static constexpr float INITIAL_BSER_BRAKE_ENGAGED_THRESHOLD = 0.25;
 
 public:
+	float apps1LoThresholdVolts;
+	float apps1HiThresholdVolts;
+	float apps2LoThresholdVolts;
+	float apps2HiThresholdVolts;
+
 	float bsefBrakeEngagedThreshold;
 	float bserBrakeEngagedThreshold;
 
