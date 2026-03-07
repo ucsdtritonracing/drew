3029b98d636debeec50a9036aacaad0b2c4f7ae9
nshie <njshie@gmail.com>
2026-03-06 19:31:37 -0800

    Modify PDU channel constants

--- vcu/App/Vehicle/Inc/vehicle_configuration.hpp
+++ vcu/App/Vehicle/Inc/vehicle_configuration.hpp
@@ -5,10 +5,9 @@ namespace vehicle {
 
 class VehicleConfiguration {
 	static constexpr uint8_t PDU_12V_CHANNEL 			= 1;
-	static constexpr uint8_t PDU_TSSI_RED_CHANNEL 		= 2;
-	static constexpr uint8_t PDU_TSSI_GREEN_CHANNEL 	= 3;
 	static constexpr uint8_t PDU_BRAKE_LIGHT_CHANNEL 	= 4;
 	static constexpr uint8_t PDU_TSB_FANS_CHANNEL 		= 5;
+	static constexpr uint8_t PDU_RADIATION_FANS_CHANNEL	= 7;
 	static constexpr uint8_t PDU_PUMPS_CHANNEL 			= 6;
 
 	static constexpr float INITIAL_APPS1_LO_THRESHOLD_VOLTAGE 	= 0.6f;
