8a10552c4a105cc6288f6c80fac752b3c82d47f9
nshie <njshie@gmail.com>
2026-04-01 10:16:55 -0700

    Implemented full control loop

--- vcu/App/Task/Src/task_control_loop.cpp
+++ vcu/App/Task/Src/task_control_loop.cpp
@@ -1,6 +1,8 @@
 #include "task.hpp"
 #include "task_control_loop.hpp"
 #include "vehicle_state.hpp"
+#include "vehicle_configuration.hpp"
+#include "vehicle_state_types.hpp"
 #include "drivers.hpp"
 #include "torque.hpp"
 #include "cmsis_os.h"
@@ -14,47 +16,71 @@ void ControlLoopTask::setup() {
 }
 
 void ControlLoopTask::loop() {
-	if (vehicle::vehicleState.getReadyToDrive()) {
-		const vehicle::Pedals pedals = vehicle::vehicleState.getPedals();
-		const float torqueCapability = vehicle::inverter.getTorqueCapability();
+	const vehicle::Mode mode = vehicle::vehicleState.getMode();
+	const vehicle::Pedals pedals = vehicle::vehicleState.getPedals();
+	const bool readyToDriveButtonPressed = vehicle::vehicleState.getReadyToDriveButtonPressed();
+	const bool shutdownCircuitClosed = vehicle::vehicleState.getShutdownCircuitClosed();
 
-		const float appCommand = pedals.app1;	// value being used for calculations
+	switch (mode) {
+	case vehicle::Mode::IDLE: {
+			const bool brakesValid = pedals.bsefValid && pedals.bserValid;
+			const bool brakePressed = pedals.bsef > vehicle::vehicleConfiguration.bsefBrakeEngagedThreshold || pedals.bser > vehicle::vehicleConfiguration.bserBrakeEngagedThreshold;
 
-		const bool appsPlausible = torque::isAPPSPlausible(pedals.app1, pedals.app2);
-		const bool appsBrakePedalPlausible = torque::isAPPSBrakePedalPlausible(appsBrakePedalPlausibilityFaulted, appCommand, pedals.bsef, pedals.bser);
+			if (brakesValid && brakePressed && readyToDriveButtonPressed && shutdownCircuitClosed) {
+				vehicle::vehicleState.setMode(vehicle::Mode::READY_TO_DRIVE);
+			}
 
-		const uint32_t currentTick = HAL_GetTick();
-
-		app1Fault.update(!pedals.app1Valid, currentTick);
-		app2Fault.update(!pedals.app2Valid, currentTick);
-		bsefFault.update(!pedals.bsefValid, currentTick);
-		bserFault.update(!pedals.bserValid, currentTick);
-		appsPlausibilityFault.update(!appsPlausible, currentTick);
-		appsBrakePedalPlausibilityFaulted = appsBrakePedalPlausible;
-
-		const bool torqueInhibited = app1Fault.torqueInhibited(currentTick) ||
-									 app2Fault.torqueInhibited(currentTick) ||
-									 bsefFault.torqueInhibited(currentTick) ||
-									 bserFault.torqueInhibited(currentTick) ||
-									 appsPlausibilityFault.torqueInhibited(currentTick) ||
-									 appsBrakePedalPlausibilityFaulted;
-
-		float torqueScalar = 0.0f;
-
-		if (!torqueInhibited) {
-			torqueScalar = torque::computeDriverTorqueRequest(appCommand);
+			vehicle::inverter.sendCommandMessage(0, false);
 		}
+		break;
+	case vehicle::Mode::READY_TO_DRIVE: {
+			if (!shutdownCircuitClosed) {
+				vehicle::vehicleState.setMode(vehicle::Mode::IDLE);
+				vehicle::inverter.sendCommandMessage(0, false);
+			} else {
+				const float torqueCapability = torque::MAX_TORQUE_LIMIT_NM;//vehicle::inverter.getTorqueCapability();
 
-		if (!std::isfinite(torqueScalar)) {
-			torqueScalar = 0.0f;
+				const float appCommand = pedals.app1;	// value being used for calculations
+
+				const bool appsPlausible = torque::isAPPSPlausible(pedals.app1, pedals.app2);
+				const bool appsBrakePedalPlausible = torque::isAPPSBrakePedalPlausible(appsBrakePedalPlausibilityFaulted, appCommand, pedals.bsef, pedals.bser);
+
+				const uint32_t currentTick = HAL_GetTick();
+
+				app1Fault.update(!pedals.app1Valid, currentTick);
+				app2Fault.update(!pedals.app2Valid, currentTick);
+				bsefFault.update(!pedals.bsefValid, currentTick);
+				bserFault.update(!pedals.bserValid, currentTick);
+				appsPlausibilityFault.update(!appsPlausible, currentTick);
+				appsBrakePedalPlausibilityFaulted = !appsBrakePedalPlausible;
+
+				const bool torqueInhibited = app1Fault.torqueInhibited(currentTick) ||
+											 app2Fault.torqueInhibited(currentTick) ||
+											 bsefFault.torqueInhibited(currentTick) ||
+											 bserFault.torqueInhibited(currentTick) ||
+											 appsPlausibilityFault.torqueInhibited(currentTick) ||
+											 appsBrakePedalPlausibilityFaulted;
+
+				float torqueScalar = 0.0f;
+
+				if (!torqueInhibited) {
+					torqueScalar = torque::computeDriverTorqueRequest(appCommand);
+				}
+
+				if (!std::isfinite(torqueScalar)) {
+					torqueScalar = 0.0f;
+				}
+
+				torqueScalar = std::clamp(torqueScalar, 0.0f, 1.0f);
+				const float driverTorqueRequestNm = torqueScalar * torque::MAX_TORQUE_LIMIT_NM;
+				const uint16_t torqueRequest = static_cast<uint16_t>(std::lroundf(std::min(driverTorqueRequestNm, torqueCapability)));
+				vehicle::inverter.sendCommandMessage(torqueRequest, true);
+			}
 		}
-
-		torqueScalar = std::clamp(torqueScalar, 0.0f, 1.0f);
-		const float driverTorqueRequestNm = torqueScalar * torque::MAX_TORQUE_LIMIT_NM;
-		const uint16_t torqueRequest = static_cast<uint16_t>(std::lroundf(std::min(driverTorqueRequestNm, torqueCapability)));
-		vehicle::inverter.sendCommandMessage(torqueRequest, true);
-	} else {
+		break;
+	default:
 		vehicle::inverter.sendCommandMessage(0, false);
+		break;
 	}
 
 	osDelay(CONTROL_LOOP_PERIOD_MS);
