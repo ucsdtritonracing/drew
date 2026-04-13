530e587dd84c4a492a7b407fd107f74234f3c856
nshie <njshie@gmail.com>
2026-04-12 18:43:56 -0700

    Removed state from CANPeripheral

--- vcu/app/drivers/can/can_peripheral.hpp
+++ vcu/app/drivers/can/can_peripheral.hpp
@@ -10,7 +10,6 @@ namespace drivers::can {
  * CAN peripheral template class.
  *
  * @param Derived Derived class type.
- * @param StateType Derived class state encapsulation struct.
  * @param canBus CAN bus this peripheral is on.
  *
  * Use bindHandler to register handlers for received messages on the CAN bus, e.g:
@@ -19,14 +18,8 @@ namespace drivers::can {
  * 		bindHandler<&Peripheral::processStatusMessage>(0x100);
  * 		bindHandler<&Peripheral::processCalibrationMessage>(0x101);
  * }
- *
- * State is double-buffered, where 'stableState' is read and 'state' is written.
- * This is to deal with non-atomic reads/writes and race conditions.
- * After an object is edited, it needs to be swapped into 'stableState'.
- * 		This is done automatically with registered handlers.
- * 		However, edits outside of a handler will need to manually call commitChanges().
  */
-template<typename Derived, typename StateType>
+template<typename Derived>
 class CANPeripheral {
 public:
 	CANPeripheral(CANBus& canBus) : canBus(canBus) {}
@@ -36,15 +29,7 @@ public:
 	CANPeripheral& operator=(const CANPeripheral&) 	= delete;
 	CANPeripheral& operator=(CANPeripheral&&)		= delete;
 
-	const StateType getState() const {
-		return stableState;
-	}
-
-private:
-	StateType stableState{};
-
 protected:
-	StateType state{};
 	CANBus& canBus;
 
 	template<void (Derived::*Method)(const Message&)>
@@ -55,13 +40,8 @@ protected:
 	        [](void *instance, const Message& message) {
 	    		Derived* d = static_cast<Derived*>(instance);
 	            (d->*Method)(message);
-	            d->commitChanges();
 	        }
 	    );
-	}
-
-	void commitChanges() {
-		std::swap(state, stableState);
 	}
 };
 
