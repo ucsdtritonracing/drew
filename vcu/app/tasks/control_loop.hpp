fe42feec45b79a68cb4633099ea05d8269260daa
nshie <65916936+nshie@users.noreply.github.com>
2026-04-25 15:07:31 -0700

    Separated control loop FSM transitions from output logic (#32)

--- vcu/app/tasks/control_loop.hpp
+++ vcu/app/tasks/control_loop.hpp
@@ -2,6 +2,7 @@
 
 #include "generics/task.hpp"
 #include "vehicle/torque.hpp"
+#include "vehicle/types/mode_types.hpp"
 
 
 namespace tasks {
@@ -14,6 +15,12 @@ public:
 private:
 	static const uint32_t CONTROL_LOOP_PERIOD_MS = 3;
 
+	struct TransitionInputs {
+		bool brakePressed;
+		bool readyToDriveButtonPressed;
+		bool shutdownCircuitClosed;
+	};
+
 	torque::TimedFault app1Fault;
 	torque::TimedFault app2Fault;
 	torque::TimedFault bsefFault;
@@ -21,6 +28,10 @@ private:
 	torque::TimedFault appsPlausibilityFault;
 
 	bool appsBrakePedalPlausibilityFaulted;
+
+	const vehicle::Mode getNextMode(vehicle::Mode currentMode, TransitionInputs inputs) const;
+	void onEnter(vehicle::Mode mode);
+	void runMode(vehicle::Mode mode);
 };
 
 } // namespace tasks
