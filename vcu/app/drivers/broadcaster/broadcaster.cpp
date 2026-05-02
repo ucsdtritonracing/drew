6fdf202b1e6cc3d97b9302c3eb4f6e70c66f2b1d
nshie <njshie@gmail.com>
2026-05-01 17:54:22 -0700

    Implemented broadcaster driver

---
+++ vcu/app/drivers/broadcaster/broadcaster.cpp
@@ -0,0 +1,87 @@
+#include "drivers/broadcaster/broadcaster.hpp"
+#include "drivers/can/can_bus.hpp"
+#include "drivers/can/can_peripheral.hpp"
+#include "vehicle/vehicle_state.hpp"
+#include "vehicle/types/wheels_types.hpp"
+#include "vehicle/types/pedals_types.hpp"
+
+
+namespace drivers::broadcaster {
+
+Broadcaster::Broadcaster(drivers::can::CANBus &canBus) :
+		drivers::can::CANPeripheral<Broadcaster>(canBus) {}
+
+
+void Broadcaster::broadcastWheelsMessage() {
+	vehicle::wheels::State wheels = vehicle::vehicleState.getWheelSpeeds();
+	int16_t frontRight	= static_cast<int16_t>(wheels.frontRight * WHEELS_SCALE);
+	int16_t frontLeft	= static_cast<int16_t>(wheels.frontLeft * WHEELS_SCALE);
+	int16_t rearRight	= static_cast<int16_t>(wheels.rearRight * WHEELS_SCALE);
+	int16_t rearLeft	= static_cast<int16_t>(wheels.rearLeft * WHEELS_SCALE);
+
+	uint16_t rawFR = static_cast<uint16_t>(frontRight);
+	uint16_t rawFL = static_cast<uint16_t>(frontLeft);
+	uint16_t rawRR = static_cast<uint16_t>(rearRight);
+	uint16_t rawRL = static_cast<uint16_t>(rearLeft);
+
+	txData[WHEELS_FR_START]		= static_cast<uint8_t>(rawFR & BYTE_MASK);
+	txData[WHEELS_FR_START+1]	= static_cast<uint8_t>((rawFR >> 8) & BYTE_MASK);
+	txData[WHEELS_FL_START]		= static_cast<uint8_t>(rawFL & BYTE_MASK);
+	txData[WHEELS_FL_START+1]	= static_cast<uint8_t>((rawFL >> 8) & BYTE_MASK);
+	txData[WHEELS_RL_START]		= static_cast<uint8_t>(rawRR & BYTE_MASK);
+	txData[WHEELS_RL_START+1]	= static_cast<uint8_t>((rawRR >> 8) & BYTE_MASK);
+	txData[WHEELS_RR_START]		= static_cast<uint8_t>(rawRL & BYTE_MASK);
+	txData[WHEELS_RR_START+1]	= static_cast<uint8_t>((rawRL >> 8) & BYTE_MASK);
+
+	canBus.transmit(CAN_ID_WHEELS_MESSAGE, txData, DLC_WHEELS_MESSAGE);
+}
+
+
+void Broadcaster::broadcastBrakesMessage() {
+	vehicle::pedals::State pedals = vehicle::vehicleState.getPedals();
+	uint16_t bsef	= static_cast<uint16_t>(pedals.bsef * PEDALS_SCALE);
+	uint16_t bser	= static_cast<uint16_t>(pedals.bser * PEDALS_SCALE);
+
+	txData[BRAKES_BSEF_START]	= static_cast<uint8_t>(bsef & BYTE_MASK);
+	txData[BRAKES_BSEF_START+1]	= static_cast<uint8_t>((bsef >> 8) & BYTE_MASK);
+	txData[BRAKES_BSER_START]	= static_cast<uint8_t>(bser & BYTE_MASK);
+	txData[BRAKES_BSER_START+1]	= static_cast<uint8_t>((bser >> 8) & BYTE_MASK);
+	txData[BRAKES_FLAGS_START]	= static_cast<uint8_t>(
+			((pedals.bsefValid & 1u) << BRAKES_BSEF_VALID_BIT) |
+			((pedals.bserValid & 1u) << BRAKES_BSER_VALID_BIT)
+	);
+	canBus.transmit(CAN_ID_BRAKES_MESSAGE, txData, DLC_BRAKES_MESSAGE);
+}
+
+
+void Broadcaster::broadcastAPPMessage() {
+	vehicle::pedals::State pedals = vehicle::vehicleState.getPedals();
+	uint16_t app1	= static_cast<uint16_t>(pedals.app1 * PEDALS_SCALE);
+	uint16_t app2	= static_cast<uint16_t>(pedals.app2 * PEDALS_SCALE);
+
+	txData[APP_APP1_START]	= static_cast<uint8_t>(app1 & BYTE_MASK);
+	txData[APP_APP1_START+1]	= static_cast<uint8_t>((app1 >> 8) & BYTE_MASK);
+	txData[APP_APP2_START]	= static_cast<uint8_t>(app2 & BYTE_MASK);
+	txData[APP_APP2_START+1]	= static_cast<uint8_t>((app2 >> 8) & BYTE_MASK);
+	txData[APP_FLAGS_START]	= static_cast<uint8_t>(
+			((pedals.app1Valid & 1u) << APP_APP1_VALID_BIT) |
+			((pedals.app2Valid & 1u) << APP_APP2_VALID_BIT)
+	);
+	canBus.transmit(CAN_ID_APP_MESSAGE, txData, DLC_APP_MESSAGE);
+}
+
+
+void Broadcaster::broadcastFlagsMessage() {
+	bool readyToDriveButtonPressed = vehicle::vehicleState.getReadyToDriveButtonPressed();
+	bool shutdownCircuitClosed = vehicle::vehicleState.getShutdownCircuitClosed();
+	bool readyToDrive = vehicle::vehicleState.getMode() == vehicle::Mode::READY_TO_DRIVE;
+
+	txData[0] = static_cast<uint8_t>(
+			((readyToDriveButtonPressed & 1u) << FLAGS_R2DB_PRESSED_BIT) |
+			((shutdownCircuitClosed & 1u) << FLAGS_SDC_CLOSED_BIT) |
+			((readyToDrive & 1u) << FLAGS_R2D_ENABLED_BIT)
+	);
+	canBus.transmit(CAN_ID_FLAGS_MESSAGE, txData, DLC_FLAGS_MESSAGE);
+}
+
+} // namespace drivers::broadcaster
---
+++ vcu/app/drivers/broadcaster/broadcaster.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "drivers/can/can_bus.hpp"
+#include "drivers/can/can_peripheral.hpp"
+#include "stm32g4xx_hal_fdcan.h"
+
+
+namespace drivers::broadcaster {
+
+class Broadcaster: public drivers::can::CANPeripheral<Broadcaster> {
+public:
+	Broadcaster(drivers::can::CANBus &canBus);
+
+	void broadcastWheelsMessage();
+	void broadcastBrakesMessage();
+	void broadcastAPPMessage();
+	void broadcastIMUMessage();
+	void broadcastFlagsMessage();
+
+private:
+	uint8_t txData[drivers::can::MAX_CLASSICAL_CAN_DATA_LENGTH] = {};
+
+	static constexpr float WHEELS_SCALE					= 100.0f;
+	static constexpr float PEDALS_SCALE					= 100.0f;
+
+	static constexpr size_t FLAGS_R2DB_PRESSED_BIT		= 0;
+	static constexpr size_t FLAGS_SDC_CLOSED_BIT		= 1;
+	static constexpr size_t FLAGS_R2D_ENABLED_BIT		= 2;
+
+	static constexpr size_t APP_APP1_START				= 0;
+	static constexpr size_t APP_APP2_START				= 2;
+	static constexpr size_t APP_FLAGS_START				= 4;
+	static constexpr size_t APP_APP1_VALID_BIT			= 0;
+	static constexpr size_t APP_APP2_VALID_BIT			= 1;
+
+	static constexpr size_t BRAKES_BSEF_START			= 0;
+	static constexpr size_t BRAKES_BSER_START			= 2;
+	static constexpr size_t BRAKES_FLAGS_START			= 4;
+	static constexpr size_t BRAKES_BSEF_VALID_BIT		= 0;
+	static constexpr size_t BRAKES_BSER_VALID_BIT		= 1;
+
+	static constexpr size_t WHEELS_FR_START				= 0;
+	static constexpr size_t WHEELS_FL_START				= 2;
+	static constexpr size_t WHEELS_RR_START				= 4;
+	static constexpr size_t WHEELS_RL_START				= 6;
+
+	static constexpr uint8_t DLC_WHEELS_MESSAGE			= FDCAN_DLC_BYTES_8;
+	static constexpr uint8_t DLC_BRAKES_MESSAGE			= FDCAN_DLC_BYTES_5;
+	static constexpr uint8_t DLC_APP_MESSAGE			= FDCAN_DLC_BYTES_5;
+	static constexpr uint8_t DLC_IMU_MESSAGE			= FDCAN_DLC_BYTES_8;
+	static constexpr uint8_t DLC_FLAGS_MESSAGE			= FDCAN_DLC_BYTES_1;
+
+	static constexpr uint32_t CAN_ID_WHEELS_MESSAGE		= 0x12A;
+	static constexpr uint32_t CAN_ID_BRAKES_MESSAGE		= 0x12B;
+	static constexpr uint32_t CAN_ID_APP_MESSAGE		= 0x12C;
+	static constexpr uint32_t CAN_ID_IMU_MESSAGE		= 0x12E;
+	static constexpr uint32_t CAN_ID_FLAGS_MESSAGE		= 0x12F;
+
+	static constexpr uint16_t BYTE_MASK					= 0xFF;
+};
+
+} // namespace drivers::broadcaster
