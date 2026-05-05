521c78d6e432c6413f5055d7bb603fe99e7f9933
nshie <njshie@gmail.com>
2026-05-05 14:56:01 -0700

    Implemented periodic timer

--- vcu/app/generics/timers.hpp
+++ vcu/app/generics/timers.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "vehicle/torque.hpp"
+#include "cmsis_os.h"
+#include "task.h"
 #include <stdint.h>
 
 
@@ -26,6 +28,26 @@ struct TimedPulse {
 	bool isActive() const {
 		return active;
 	}
+};
+
+
+class PeriodicTimer {
+public:
+	PeriodicTimer(TickType_t periodMs)
+		: period(pdMS_TO_TICKS(periodMs)), lastWakeTime(0), initialized(false) {}
+
+	void wait() {
+		if (!initialized) {
+			lastWakeTime = xTaskGetTickCount();
+			initialized = true;
+		}
+		vTaskDelayUntil(&lastWakeTime, period);
+	}
+
+private:
+	TickType_t period;
+	TickType_t lastWakeTime;
+	bool initialized;
 };
 
 } // namespace timers
