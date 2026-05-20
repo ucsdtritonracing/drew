8ed6ef6e1a59efe2a3b66dcad9603b21a38ae0aa
nshie <njshie@gmail.com>
2026-05-20 15:07:27 -0700

    Added hold time before R2DB press registers

--- vcu/app/tasks/polling.hpp
+++ vcu/app/tasks/polling.hpp
@@ -2,16 +2,23 @@
 
 #include "rtos/task.hpp"
 #include "generics/timers.hpp"
+#include "FreeRTOS.h"
 
 
 namespace rtos::tasks {
 
 class PollingTask : public Task<PollingTask, osPriorityLow, 128> {
 public:
+	void init();
 	void loop();
 
 private:
-	static const uint32_t DELAY = 100;
+	static constexpr uint32_t R2DB_HOLD_MS = 1000;
+	bool r2dbPressedPrev;
+	uint32_t r2dbPressTick;
+
+
+	static constexpr uint32_t DELAY = 100;
 	timers::PeriodicTimer loopTimer{DELAY};
 };
 
