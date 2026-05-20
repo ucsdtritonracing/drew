f1ac468c746cd49088d6ec281ab723865f8509f3
nshie <njshie@gmail.com>
2026-05-19 17:26:39 -0700

    Activated pull down on R2DS output

--- vcu/Core/Src/main.cpp
+++ vcu/Core/Src/main.cpp
@@ -483,18 +483,25 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin R2D_Sound_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin|R2D_Sound_Pin;
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : R2D_Button_Pin */
   GPIO_InitStruct.Pin = R2D_Button_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(R2D_Button_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : R2D_Sound_Pin */
+  GPIO_InitStruct.Pin = R2D_Sound_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(R2D_Sound_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : SDC_IN_Pin */
   GPIO_InitStruct.Pin = SDC_IN_Pin;
