40d633aa6bc69e226c114f3d4b8e1755c266f546
nshie <65916936+nshie@users.noreply.github.com>
2026-05-21 04:31:51 -0700

    Require held R2DB (#46)

--- vcu/app/tasks/polling.cpp
+++ vcu/app/tasks/polling.cpp
@@ -7,14 +7,28 @@
 
 namespace rtos::tasks {
 
+void PollingTask::init() {
+	r2dbPressedPrev = false;
+}
+
 void PollingTask::loop() {
-	uint32_t read;
+	uint32_t now = HAL_GetTick();
+	bool r2dbPressedCurrently = HAL_GPIO_ReadPin(R2D_Button_GPIO_Port, R2D_Button_Pin) == GPIO_PIN_RESET;	// pressed = GND (RESET)
 
-	read = HAL_GPIO_ReadPin(R2D_Button_GPIO_Port, R2D_Button_Pin);
-	vehicle::vehicleState.setReadyToDriveButtonPressed(!read);
+	bool r2dbHeld = false;
+	if (r2dbPressedCurrently && !r2dbPressedPrev) {
+		r2dbPressTick = now;
+	}
+	if (r2dbPressedCurrently && (now - r2dbPressTick) > R2DB_HOLD_MS) {
+		r2dbHeld = true;
+	}
 
-	read = HAL_GPIO_ReadPin(SDC_IN_GPIO_Port, SDC_IN_Pin);
-	vehicle::vehicleState.setShutdownCircuitClosed(read);
+	r2dbPressedPrev = r2dbPressedCurrently;
+	vehicle::vehicleState.setReadyToDriveButtonPressed(r2dbHeld);
+
+
+	GPIO_PinState sdcClosed = HAL_GPIO_ReadPin(SDC_IN_GPIO_Port, SDC_IN_Pin);
+	vehicle::vehicleState.setShutdownCircuitClosed(sdcClosed);
 
 	loopTimer.wait();
 }
