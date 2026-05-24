e45cc79e228b5207f1b37e8835a460e54344205b
nshie <njshie@gmail.com>
2026-05-23 17:24:27 -0700

    Declare and construct configurator driver in drivers

--- vcu/app/vehicle/drivers.cpp
+++ vcu/app/vehicle/drivers.cpp
@@ -1,10 +1,4 @@
 #include "vehicle/drivers.hpp"
-#include "drivers/can/can_bus.hpp"
-#include "drivers/sas/sas.hpp"
-#include "drivers/pdu/pdu.hpp"
-#include "drivers/pedals/pedals.hpp"
-#include "drivers/broadcaster/broadcaster.hpp"
-#include "drivers/sound/sound.hpp"
 
 
 namespace vehicle {
@@ -17,5 +11,6 @@ drivers::inverter::Inverter inverterDriver(CANBus1);
 drivers::pedals::Pedals pedalsDriver;
 drivers::broadcaster::Broadcaster broadcasterDriver(CANBus1);
 drivers::sound::Sound soundDriver;
+drivers::configurator::Configurator configuratorDriver(CANBus1);
 
 } // namespace vehicle
--- vcu/app/vehicle/drivers.hpp
+++ vcu/app/vehicle/drivers.hpp
@@ -7,6 +7,7 @@
 #include "drivers/pedals/pedals.hpp"
 #include "drivers/broadcaster/broadcaster.hpp"
 #include "drivers/sound/sound.hpp"
+#include "drivers/configurator/configurator.hpp"
 
 
 namespace vehicle {
@@ -19,5 +20,6 @@ extern drivers::inverter::Inverter inverterDriver;
 extern drivers::pedals::Pedals pedalsDriver;
 extern drivers::broadcaster::Broadcaster broadcasterDriver;
 extern drivers::sound::Sound soundDriver;
+extern drivers::configurator::Configurator configuratorDriver;
 
 } // namespace vehicle
