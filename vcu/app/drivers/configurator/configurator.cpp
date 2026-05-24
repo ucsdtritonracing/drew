e2c3c9298c7bc6041e3bf4bd51a4b7d81d10e757
nshie <njshie@gmail.com>
2026-05-23 17:23:20 -0700

    Implemented configurator

--- vcu/app/drivers/configurator/configurator.cpp
+++ vcu/app/drivers/configurator/configurator.cpp
@@ -1,6 +1,9 @@
 #include "drivers/configurator/configurator.hpp"
 #include "vehicle/vehicle_state.hpp"
 #include "vehicle/vehicle_configuration.hpp"
+#include "vehicle/types/configuration_types.hpp"
+#include "vehicle/torque.hpp"
+#include <string.h>
 
 
 namespace drivers::configurator {
@@ -8,77 +11,319 @@ namespace drivers::configurator {
 Configurator::Configurator(drivers::can::CANBus& canBus)
     : CANPeripheral(canBus) {}
 
+
 void Configurator::init() {
 	bindHandler<&Configurator::processCommandSetParameter>(CAN_ID_SET_PARAMETER);
-	bindHandler<&Configurator::processCommandGetParameter>(CAN_ID_GET_PARAMETER);
 	parameterResponseSlotHandle = bindTxSlot(CAN_ID_PARAMETER_RESPONSE, drivers::can::STATUS);
 }
 
-Parameter Configurator::parseParameterId(const drivers::can::Message& message) {
-	uint16_t rawId = static_cast<uint16_t>(message.data[0]) | (static_cast<uint16_t>(message.data[1]) << 8);
 
-	switch (rawId) {
-	case static_cast<Parameter>(Parameter::APP1_THRESHOLD):
-	case static_cast<Parameter>(Parameter::APP2_THRESHOLD):
-	case static_cast<Parameter>(Parameter::BSEF_THRESHOLD):
-	case static_cast<Parameter>(Parameter::BSER_THRESHOLD):
-	case static_cast<Parameter>(Parameter::MAXIMUM_TORQUE_REQUEST_NM):
-	case static_cast<Parameter>(Parameter::PEDAL_MAP):
-		return static_cast<Parameter>(rawId);
-	default:
-		return Parameter::INVALID;
+void Configurator::setMaxTorque(float value) {
+	if (value < 0 || value > torque::MAX_TORQUE_LIMIT_NM) {
+		return;
 	}
+	stagedConfiguration.maxTorqueNm = value;
 }
 
-void Configurator::processCommandSetParameter(const drivers::can::Message& message) {
-	if (message.numBytes != 8) {
+
+bool Configurator::requestingConfigurationMode() const {
+	return configurationModeRequested;
+}
+
+
+void Configurator::processCommandWriteConfiguration(const drivers::can::Message& message) {
+	if (message.numBytes != WRITE_CONFIGURATION_MESSAGE_NUM_BYTES) {
 		return;
 	}
 
-	const Parameter parameter = parseParameterId(message);
+	if (stagedConfiguration.valid() && vehicle::vehicleState.getMode() == vehicle::Mode::CONFIGURATION) {
+		vehicle::vehicleConfiguration = stagedConfiguration;
+	}
+}
 
-	const uint32_t rawData =	static_cast<uint32_t>(message.data[0]) |
-								static_cast<uint32_t>(message.data[1]) << 8 |
-								static_cast<uint32_t>(message.data[2]) << 16 |
-								static_cast<uint32_t>(message.data[3]) << 24;
 
-	const float data = reinterpret_cast<float>(rawData);
+void Configurator::processCommandConfigurationMode(const drivers::can::Message& message) {
+	if (message.numBytes != CONFIGURATION_MODE_MESSAGE_NUM_BYTES) {
+		return;
+	}
+	configurationModeRequested = message.data[0];
+}
 
-	const uint8_t index	= message.data[6];
-	const uint8_t type	= message.data[7];
-	const uint16_t bigIndex = static_cast<uint16_t>(message.data[6]) | static_cast<uint16_t>(message.data[7]) << 8;
+
+void Configurator::processCommandSetParameter(const drivers::can::Message& message) {
+	if (vehicle::vehicleState.getMode() != vehicle::Mode::CONFIGURATION) {
+		return;
+	}
+
+	if (message.numBytes != PARAMETER_MESSAGE_NUM_BYTES) {
+		return;
+	}
+
+	const std::optional<Parameter> parameter = parseParameterId(message);
+	if (!parameter) {
+		return;
+	}
+
+	const uint32_t rawData =	static_cast<uint32_t>(message.data[PARAMETER_MESSAGE_DATA_START])			|
+								static_cast<uint32_t>(message.data[PARAMETER_MESSAGE_DATA_START + 1]) << 8	|
+								static_cast<uint32_t>(message.data[PARAMETER_MESSAGE_DATA_START + 2]) << 16 |
+								static_cast<uint32_t>(message.data[PARAMETER_MESSAGE_DATA_START + 3]) << 24;
+	float value = unscale(rawData);
+
+	const uint16_t info =	static_cast<uint16_t>(message.data[PARAMETER_MESSAGE_INFO_START]) |
+							static_cast<uint16_t>(message.data[PARAMETER_MESSAGE_INFO_START + 1]) << 8;
+
+	switch (*parameter) {
+	case Parameter::MAX_TORQUE_REQUEST: {
+		setMaxTorque(value);
+		break;
+	}
+	case Parameter::PEDAL_MAP: {
+		stagedConfiguration.pedalMap.setPoint(info, value);
+		break;
+	}
+	case Parameter::APP1_THRESHOLD:
+	case Parameter::APP2_THRESHOLD:
+	case Parameter::BSEF_THRESHOLD:
+	case Parameter::BSER_THRESHOLD: {
+		std::optional<ThresholdType> type = parseThresholdType(info);
+		if (type.has_value()) {
+			setThreshold(*parameter, *type, value);
+		}
+		break;
+	}
+	default:
+		break;
+	}
+}
+
+
+void Configurator::setThreshold(Parameter parameter, ThresholdType type, float value) {
+	vehicle::AnalogCalibration* calibration = nullptr;
 
 	switch (parameter) {
 	case Parameter::APP1_THRESHOLD:
-		switch (index) {
-		case ThresholdIndex::FAULT_LOW:
-			stagedConfiguration.app1Thresholds.faultThresholds.VoltageRange(min, max)
-		}
-
+		calibration = &stagedConfiguration.app1Thresholds;
 		break;
 	case Parameter::APP2_THRESHOLD:
+		calibration = &stagedConfiguration.app2Thresholds;
 		break;
 	case Parameter::BSEF_THRESHOLD:
+		if (type == ThresholdType::ENGAGE) {
+			stagedConfiguration.bsefBrakeEngagedThreshold = value;
+			return;
+		}
+		calibration = &stagedConfiguration.bsefThresholds;
 		break;
 	case Parameter::BSER_THRESHOLD:
-		break;
-	case Parameter::MAXIMUM_TORQUE_REQUEST_NM:
-		break;
-	case Parameter::PEDAL_MAP:
+		if (type == ThresholdType::ENGAGE) {
+			stagedConfiguration.bserBrakeEngagedThreshold = value;
+			return;
+		}
+		calibration = &stagedConfiguration.bserThresholds;
 		break;
 	default:
 		return;
 	}
+
+	vehicle::Range* range = nullptr;
+	if (type == ThresholdType::FAULT_LOW || type == ThresholdType::FAULT_HIGH) {
+		range = &calibration->faultThresholds;
+	} else {
+		range = &calibration->signalThresholds;
+	}
+
+	if (type == ThresholdType::FAULT_HIGH || type == ThresholdType::SIGNAL_HIGH) {
+		*range = {range->getMin(), value};
+	} else {
+		*range = {value, range->getMax()};
+	}
 }
 
-void Configurator::processCommandGetParameter(const drivers::can::Message& message) {
-	if (message.numBytes != 8) {
+
+void Configurator::sendThresholdRanges(Parameter parameter) {
+	vehicle::AnalogCalibration* calibration = nullptr;
+	switch (parameter) {
+	case Parameter::APP1_THRESHOLD:
+		calibration = &stagedConfiguration.app1Thresholds;
+		break;
+	case Parameter::APP2_THRESHOLD:
+		calibration = &stagedConfiguration.app2Thresholds;
+		break;
+	case Parameter::BSEF_THRESHOLD:
+		calibration = &stagedConfiguration.bsefThresholds;
+		break;
+	case Parameter::BSER_THRESHOLD:
+		calibration = &stagedConfiguration.bserThresholds;
+		break;
+	default:
+		return;
+	}
+	sendParameterValue(parameter, static_cast<uint16_t>(ThresholdType::FAULT_LOW), calibration->faultThresholds.getMin());
+	sendParameterValue(parameter, static_cast<uint16_t>(ThresholdType::FAULT_HIGH), calibration->faultThresholds.getMax());
+	sendParameterValue(parameter, static_cast<uint16_t>(ThresholdType::SIGNAL_LOW), calibration->signalThresholds.getMin());
+	sendParameterValue(parameter, static_cast<uint16_t>(ThresholdType::SIGNAL_HIGH), calibration->signalThresholds.getMax());
+}
+
+
+void Configurator::sendParameterValue(Parameter parameter, uint16_t info, float data) {
+	uint16_t rawParameter = static_cast<uint16_t>(parameter);
+	txData[0] = rawParameter;
+	txData[1] = rawParameter >> 8;
+
+	txData[2] = info;
+	txData[3] = info >> 8;
+
+	uint32_t scaledData = scale(data);
+	txData[4] = scaledData;
+	txData[5] = scaledData >> 8;
+	txData[6] = scaledData >> 16;
+	txData[7] = scaledData >> 24;
+
+	canBus.publishTxSlot(parameterResponseSlotHandle, txData, PARAMETER_MESSAGE_NUM_BYTES);
+}
+
+
+void Configurator::broadcastNextConfigurationParameter() {
+	if (vehicle::vehicleState.getMode() != vehicle::Mode::CONFIGURATION) {
 		return;
 	}
 
-	const Parameter parameter = parseParameterId(message);
+	switch (broadcastParameterIndex) {
+	// APP1
+	case 0:
+		sendParameterValue(Parameter::APP1_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_LOW), stagedConfiguration.app1Thresholds.faultThresholds.getMin());
+		break;
+	case 1:
+		sendParameterValue(Parameter::APP1_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_HIGH), stagedConfiguration.app1Thresholds.faultThresholds.getMax());
+		break;
+	case 2:
+		sendParameterValue(Parameter::APP1_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_LOW), stagedConfiguration.app1Thresholds.signalThresholds.getMin());
+		break;
+	case 3:
+		sendParameterValue(Parameter::APP1_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_HIGH), stagedConfiguration.app1Thresholds.signalThresholds.getMax());
+		break;
+
+	// APP2
+	case 4:
+		sendParameterValue(Parameter::APP2_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_LOW), stagedConfiguration.app2Thresholds.faultThresholds.getMin());
+		break;
+	case 5:
+		sendParameterValue(Parameter::APP2_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_HIGH), stagedConfiguration.app2Thresholds.faultThresholds.getMax());
+		break;
+	case 6:
+		sendParameterValue(Parameter::APP2_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_LOW), stagedConfiguration.app2Thresholds.signalThresholds.getMin());
+		break;
+	case 7:
+		sendParameterValue(Parameter::APP2_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_HIGH), stagedConfiguration.app2Thresholds.signalThresholds.getMax());
+		break;
+
+	// BSEF
+	case 8:
+		sendParameterValue(Parameter::BSEF_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_LOW), stagedConfiguration.bsefThresholds.faultThresholds.getMin());
+		break;
+	case 9:
+		sendParameterValue(Parameter::BSEF_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_HIGH), stagedConfiguration.bsefThresholds.faultThresholds.getMax());
+		break;
+	case 10:
+		sendParameterValue(Parameter::BSEF_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_LOW), stagedConfiguration.bsefThresholds.signalThresholds.getMin());
+		break;
+	case 11:
+		sendParameterValue(Parameter::BSEF_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_HIGH), stagedConfiguration.bsefThresholds.signalThresholds.getMax());
+		break;
+	case 12:
+		sendParameterValue(Parameter::BSEF_THRESHOLD, static_cast<uint16_t>(ThresholdType::ENGAGE), stagedConfiguration.bsefBrakeEngagedThreshold);
+		break;
+
+	// BSER
+	case 13:
+		sendParameterValue(Parameter::BSER_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_LOW), stagedConfiguration.bserThresholds.faultThresholds.getMin());
+		break;
+	case 14:
+		sendParameterValue(Parameter::BSER_THRESHOLD, static_cast<uint16_t>(ThresholdType::FAULT_HIGH), stagedConfiguration.bserThresholds.faultThresholds.getMax());
+		break;
+	case 15:
+		sendParameterValue(Parameter::BSER_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_LOW), stagedConfiguration.bserThresholds.signalThresholds.getMin());
+		break;
+	case 16:
+		sendParameterValue(Parameter::BSER_THRESHOLD, static_cast<uint16_t>(ThresholdType::SIGNAL_HIGH), stagedConfiguration.bserThresholds.signalThresholds.getMax());
+		break;
+	case 17:
+		sendParameterValue(Parameter::BSER_THRESHOLD, static_cast<uint16_t>(ThresholdType::ENGAGE), stagedConfiguration.bserBrakeEngagedThreshold);
+		break;
+
+	// max torque request
+	case 18:
+		sendParameterValue(Parameter::MAX_TORQUE_REQUEST, 0, stagedConfiguration.maxTorqueNm);
+		break;
+
+	// pedal map
+	case 19:
+	case 20:
+	case 21:
+	case 22:
+	case 23:
+	case 24:
+	case 25:
+	case 26:
+	case 27:
+	case 28:
+	case 29:
+	case 30:
+	case 31:
+	case 32:
+	case 33:
+	case 34:
+		sendParameterValue(Parameter::PEDAL_MAP, broadcastParameterIndex - 18, stagedConfiguration.pedalMap.getPoint(broadcastParameterIndex - 18));
+		break;
+
+	default:
+		break;
+	}
+
+	broadcastParameterIndex = (broadcastParameterIndex + 1) % NUM_BROADCAST_PARAMETER_VALUES;
+}
 
 
+float Configurator::unscale(uint32_t data) {
+	return static_cast<float>(static_cast<int32_t>(data)) / PARAMETER_MESSAGE_SCALE;
+}
+
+
+uint32_t Configurator::scale(float value) {
+	int32_t scaled = static_cast<int32_t>(value * PARAMETER_MESSAGE_SCALE);
+	return static_cast<uint32_t>(scaled);
+}
+
+
+std::optional<Parameter> Configurator::parseParameterId(const drivers::can::Message& message) {
+	uint16_t rawId = static_cast<uint16_t>(message.data[0]) | (static_cast<uint16_t>(message.data[1]) << 8);
+
+	switch (rawId) {
+	case static_cast<uint16_t>(Parameter::APP1_THRESHOLD):
+	case static_cast<uint16_t>(Parameter::APP2_THRESHOLD):
+	case static_cast<uint16_t>(Parameter::BSEF_THRESHOLD):
+	case static_cast<uint16_t>(Parameter::BSER_THRESHOLD):
+	case static_cast<uint16_t>(Parameter::MAX_TORQUE_REQUEST):
+	case static_cast<uint16_t>(Parameter::PEDAL_MAP):
+		return static_cast<Parameter>(rawId);
+	default:
+		return std::nullopt;
+	}
+}
+
+
+std::optional<ThresholdType> Configurator::parseThresholdType(uint16_t info) {
+	switch (info) {
+	case static_cast<uint16_t>(ThresholdType::FAULT_LOW):
+	case static_cast<uint16_t>(ThresholdType::FAULT_HIGH):
+	case static_cast<uint16_t>(ThresholdType::SIGNAL_LOW):
+	case static_cast<uint16_t>(ThresholdType::SIGNAL_HIGH):
+	case static_cast<uint16_t>(ThresholdType::ENGAGE):
+		return static_cast<ThresholdType>(info);
+	default:
+		return std::nullopt;
+	}
 }
 
 } // namespace drivers::configurator
--- vcu/app/drivers/configurator/configurator.hpp
+++ vcu/app/drivers/configurator/configurator.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
-#include "drivers/configurator/parameter.hpp"
+#include "drivers/configurator/configurator_types.hpp"
 #include "drivers/can/can_bus.hpp"
 #include "drivers/can/can_peripheral.hpp"
 #include "drivers/can/tx_slot.hpp"
 #include "vehicle/vehicle_configuration.hpp"
+#include <optional>
 
 
 namespace drivers::configurator {
@@ -16,20 +17,48 @@ public:
 	void init();
 
 	void processCommandSetParameter(const drivers::can::Message& message);
-	void processCommandGetParameter(const drivers::can::Message& message);
-	void processCommandPushConfiguration(const drivers::can::Message& message);
+	void processCommandWriteConfiguration(const drivers::can::Message& message);
+	void processCommandConfigurationMode(const drivers::can::Message& message);
+
+	bool requestingConfigurationMode() const;
+	void broadcastNextConfigurationParameter();
 
 private:
-	Parameter parseParameterId(const drivers::can::Message& message);
-	void sendParameterValue(Parameter parameter, uint32_t data, uint16_t info);
+    vehicle::VehicleConfiguration stagedConfiguration;
+	bool configurationModeRequested = false;
+	int broadcastParameterIndex = 0;
+	static constexpr int NUM_BROADCAST_PARAMETER_VALUES = 34;
 
 	drivers::can::TxSlotHandle parameterResponseSlotHandle;
+	uint8_t txData[can::MAX_CLASSICAL_CAN_DATA_LENGTH];
 
-    static constexpr uint32_t CAN_ID_SET_PARAMETER		= 0x190;
-    static constexpr uint32_t CAN_ID_GET_PARAMETER		= 0x191;
-    static constexpr uint32_t CAN_ID_PARAMETER_RESPONSE	= 0xCCC;
+    static constexpr uint32_t CAN_ID_SET_PARAMETER					= 0x190;
+    static constexpr uint32_t CAN_ID_PARAMETER_RESPONSE				= 0xCCC;
+    static constexpr uint32_t CAN_ID_CONFIGURATION_MODE				= 0x193;
+    static constexpr uint32_t CAN_ID_WRITE_CONFIGURATION			= 0x194;
 
-    vehicle::VehicleConfiguration stagedConfiguration;
+    static constexpr size_t PARAMETER_MESSAGE_DATA_START			= 4;
+    static constexpr size_t PARAMETER_MESSAGE_INFO_START			= 2;
+    static constexpr size_t PARAMETER_MESSAGE_NUM_BYTES				= 8;
+    static constexpr float PARAMETER_MESSAGE_SCALE					= 1000000;
+
+    static constexpr size_t CONFIGURATION_MODE_MESSAGE_NUM_BYTES	= 1;
+
+    static constexpr size_t WRITE_CONFIGURATION_MESSAGE_NUM_BYTES	= 1;
+
+
+	std::optional<Parameter> parseParameterId(const drivers::can::Message& message);
+	std::optional<ThresholdType> parseThresholdType(uint16_t info);
+
+	uint32_t scale(float value);
+	float unscale(uint32_t data);
+
+	void setThreshold(Parameter parameter, ThresholdType type, float value);
+	void setMaxTorque(float value);
+
+	void sendParameterValue(Parameter parameter, uint16_t info, float data);
+	void sendThresholdRanges(Parameter parameter);
+
 };
 
 } // namespace drivers::configurator
