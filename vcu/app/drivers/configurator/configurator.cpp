5b0fd62706eeb47dcf07dac7b57a141796018c12
nshie <njshie@gmail.com>
2026-05-29 08:46:55 -0700

    Copy vehicle configuration to staged configuration when enabling config mode

--- vcu/app/drivers/configurator/configurator.cpp
+++ vcu/app/drivers/configurator/configurator.cpp
@@ -48,7 +48,13 @@ void Configurator::processCommandConfigurationMode(const drivers::can::Message& 
 	if (message.numBytes != CONFIGURATION_MODE_MESSAGE_NUM_BYTES) {
 		return;
 	}
-	configurationModeRequested = static_cast<bool>(message.data[0]);
+
+	bool enable = message.data[0] != 0;
+
+	if (!configurationModeRequested && enable) {
+		stagedConfiguration = vehicle::vehicleConfiguration;
+	}
+	configurationModeRequested = enable;
 }
 
 
