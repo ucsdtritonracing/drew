06a203d7daa7faf900cbf184cda3679808272a83
nshie <njshie@gmail.com>
2026-06-10 19:04:38 -0700

    Update validity checks for configuration types

--- vcu/app/vehicle/types/configuration_types.cpp
+++ vcu/app/vehicle/types/configuration_types.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "vehicle/types/configuration_types.hpp"
 #include "vehicle/torque.hpp"
 
@@ -27,7 +25,7 @@ bool PedalsConfig::valid() const {
 }
 
 bool WheelsConfig::valid() const {
-	if (!frontWheelMetersPerRevolution <= 0 || !rearWheelMetersPerRevolution <= 0) {
+	if (frontWheelMetersPerRevolution <= 0 || rearWheelMetersPerRevolution <= 0) {
 		return false;
 	}
 	if (wheelSpeedSensorAlpha <= 0 || wheelSpeedSensorAlpha > 1) {
@@ -40,6 +38,35 @@ bool TorqueConfig::valid() const {
 	if (maxTorqueNm < 0 || maxTorqueNm > torque::MAX_TORQUE_LIMIT_NM) {
 		return false;
 	}
+	return true;
+}
+
+bool PDUConfig::valid() const {
+	if (PDU_12V_RIGHT_CHANNEL.channel == 0 || PDU_12V_RIGHT_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_12V_MAIN_CHANNEL.channel == 0 || PDU_12V_MAIN_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_TSB_FANS_CHANNEL.channel == 0 || PDU_TSB_FANS_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_RADIATOR_FANS_CHANNEL.channel == 0 || PDU_RADIATOR_FANS_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_PUMPS_CHANNEL.channel == 0 || PDU_PUMPS_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_BRAKE_LIGHT_CHANNEL.channel == 0 || PDU_BRAKE_LIGHT_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_UNUSED_CHANNEL.channel == 0 || PDU_UNUSED_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+	if (PDU_12V_LEFT_CHANNEL.channel == 0 || PDU_12V_LEFT_CHANNEL.channel > vehicle::pdu::NUM_CHANNELS) {
+	    return false;
+	}
+
 	return true;
 }
 
